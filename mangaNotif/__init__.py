4b1df58b33ba1ae78f628b2f861817b9cf111efc
Harshit Jain <hj.harshit007@gmail.com>
2017-12-15 14:20:15 +0530

    Added flask-admin

--- mangaNotif/__init__.py
+++ mangaNotif/__init__.py
@@ -4,6 +4,8 @@ from flask_bcrypt import Bcrypt
 from flask_marshmallow import Marshmallow
 from flask_apscheduler import APScheduler
 from flask_cors import CORS
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -14,9 +16,14 @@ ma = Marshmallow(app)
 bcrypt = Bcrypt(app)
 scheduler = APScheduler()
 scheduler.init_app(app)
+admin = Admin(app, template_mode='bootstrap3')
 
 from views import *
 from models import *
 from urls import *
 
 db.create_all()
+
+admin.add_view(ModelView(User, db.session))
+admin.add_view(ModelView(Manga, db.session))
+admin.add_view(ModelView(UserManga, db.session))
