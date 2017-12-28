19633be683abd823caf2485d9f62d7b5c347e387
Harshit Jain <hj.harshit007@gmail.com>
2017-12-28 14:44:39 +0530

    Changed cron time

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ JOBS = [
         'func': 'mangaNotif.helper_functions:scrape_manga_data',
         'trigger': 'cron',
         'day_of_week': 'mon-sun',
-        'hour': 8,
-        'minute': 55
+        'hour': 9,
+        'minute': 18
     }
 ]
